fade5db8f002cc0b9dfadd8b97f26946932c12e1
mhsakarya <sakaryamh@gmail.com>
2022-02-15 11:26:43 +0300

    Add files via upload
    paramters updated

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
-SHORT_BREAK_MIN = 1
-LONG_BREAK_MIN = 1
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 reps = 0
 timer = None
 
@@ -25,9 +25,9 @@ def reset_timer():
 def start_timer():
     global reps
     reps += 1
-    work_sec = WORK_MIN*10
-    short_break_sec = SHORT_BREAK_MIN*10
-    long_break_sec = LONG_BREAK_MIN*10
+    work_sec = WORK_MIN*60
+    short_break_sec = SHORT_BREAK_MIN*60
+    long_break_sec = LONG_BREAK_MIN*60
     if reps % 8 == 0:
         count_down(long_break_sec)
         label_timer.config(text="Long Break", fg=RED)
@@ -66,7 +66,7 @@ def count_down(count):
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=10, pady=5, bg=YELLOW)
 
 canvas = Canvas(width=200, height=233, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="pomodoro.png")
